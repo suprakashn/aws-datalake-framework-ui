186f7944de5cc3eeacae8d5d7e60435e7b55301c
Divya Immanni <divya.immanni@tigeranalytics.com>
2022-08-22 11:59:32 +0530

    Environment variables for prod and dev

--- src/routes/defaultInstance.js
+++ src/routes/defaultInstance.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-const defaultInstance= axios.create({baseURL:'https://gcknss7eyl.execute-api.us-east-2.amazonaws.com/test/'});
+// const defaultInstance= axios.create({baseURL:'https://gcknss7eyl.execute-api.us-east-2.amazonaws.com/test/'});
+
+const baseUrl = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_BASE_URL : process.env.REACT_APP_PROD_BASE_URL
+
+const defaultInstance= axios.create({baseURL:baseUrl });
 
 export default defaultInstance;
