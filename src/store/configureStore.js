8b39e0f5ec702828f6932b1cc8c7ad4a038c4b0a
Divya Immanni <divya.immanni@tigeranalytics.com>
2022-08-24 12:29:09 +0530

    Url update

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,13 +5,13 @@ import asyncAwait from 'redux-async-await';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 
-const env= window.location.host.includes('localhost') ? 'development' : 'production';
+//const env= window.location.host.includes('localhost') ? 'development' : 'production';
 const middleware = [thunk];
 const theme = window.location.pathname.split('/')[1];
 const basename = `/${theme}/`;
 const history = createBrowserHistory({basename});
 
-if (env === 'development') {
+if (process.env.NODE_ENV === 'development') {
     middleware.push(
         createLogger({
             collapsed: (getState , action) => true,
